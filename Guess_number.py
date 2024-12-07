ef92b4ec7c70989c69b72b0fec5a3dd38b9f7b34
debjit11 <ddebjit421@gmail.com>
2024-12-07 23:19:34 +0530

    Added Guess the Number game

---
+++ Guess_number.py
@@ -0,0 +1,29 @@
+# import random
+
+# computer = random.randint(1,100)
+# number = -1
+# Guessnumber = 1
+# while(number!= computer):
+#     number = int(input("Guess the number: "))
+#     if(number>computer):
+#         print("Guess lower number...")
+#         Guessnumber += 1
+#     elif(number<computer):
+#         print("Guess higher number...")
+#     Guessnumber += 1
+# print(f"Computer Guess the number {computer} and you get this number {Guessnumber}")
+
+import random
+computer = random.randint(1,100)
+number = -1
+Guessnumber = 1
+while(number != computer):
+    number = int(input("Guess the number: "))
+    if(number>computer):
+        print("Guess the lower number...")
+         
+        Guessnumber += 1
+    elif(number<computer):
+        print("Guess higher number...")
+        Guessnumber += 1
+print(f"Computer Guess the number {computer} and you get this number {Guessnumber} attemps.")
